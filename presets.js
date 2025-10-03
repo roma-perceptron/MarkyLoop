35cc76453a65b70f9d4ad14a6a487a5516586263
roma-perceptron <roma.perceptron@gmail.com>
2025-10-03 23:27:14 +0300

    Add pre-built presets

---
+++ presets.js
@@ -0,0 +1,97 @@
+const presets = {
+  "Blues": {
+    "videoId": "9s38vkIn374",
+    "title": "Блюз",
+    "timecodes": [
+      {
+        "id": 1757253438745,
+        "name": "Два такта",
+        "start": 34.35,
+        "end": 44,
+        "mode": 1
+      },
+      {
+        "id": 1757253769652,
+        "name": "Весь блюз",
+        "start": 34.35,
+        "end": 85.5,
+        "mode": 1
+      }
+    ]
+  },
+  "BoomBoom": {
+    "videoId": "nUUyFrHERpU",
+    "title": "BoomBoom",
+    "timecodes": [
+      {
+        "id": 1757232945214,
+        "name": "Целиком",
+        "start": 0,
+        "end": 13.25,
+        "mode": 1
+      },
+      {
+        "id": 1757234023748,
+        "name": "Только вторая часть",
+        "start": 6.75,
+        "end": 13.25,
+        "mode": 1
+      }
+    ]
+  },
+  "Highwaystar solo": {
+    "videoId": "DI4SBMzCP5c",
+    "title": "HighwayStar",
+    "timecodes": [
+      {
+        "id": 1757235423420,
+        "name": "Часть 1",
+        "start": 7.25,
+        "end": 32.35,
+        "mode": 1
+      },
+      {
+        "id": 1757237053006,
+        "name": "Часть 2, начало",
+        "start": 33,
+        "end": 43.5,
+        "mode": 1
+      },
+      {
+        "id": 1757237388243,
+        "name": "Часть 2, конец",
+        "start": 43.5,
+        "end": 56,
+        "mode": 1
+      },
+      {
+        "id": 1757237683418,
+        "name": "Часть 2 целиком",
+        "start": 33,
+        "end": 56,
+        "mode": 1
+      },
+      {
+        "id": 1757237810320,
+        "name": "Часть 3, начало",
+        "start": 56,
+        "end": 80,
+        "mode": 1
+      },
+      {
+        "id": 1757238149324,
+        "name": "Часть 3, целиком",
+        "start": 56,
+        "end": 104.2,
+        "mode": 1
+      },
+      {
+        "id": 1757865174508,
+        "name": "Часть 4",
+        "start": 105.2,
+        "end": 128.4,
+        "mode": 1
+      }
+    ]
+  }
+};
